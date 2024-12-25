bf5995214a31a338f3eb76ec8552e6df05328310
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 10:33:42 +0300

    add variable declarations and comments in 1_Variables.js

--- 1_Variables.js
+++ 1_Variables.js
@@ -1,2 +1,11 @@
 
-# 
+/*Variables in Javascript are containers that store data values.*/
+/*they are declared using var, let or const keyword*/
+/*var is function scoped, let and const are block scoped*/
+/*let and const are introduced in ES6*/
+
+var name = "John";
+let age = 25;
+const country = "India";
+
+console.log(name);
