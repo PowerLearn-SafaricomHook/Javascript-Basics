40181d4930fa8a7c25f4c909103ee3daa533a292
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 10:44:29 +0300

    add initial structure and introductory comment for 2_DataTypes.js

---
+++ 2_DataTypes.js
@@ -0,0 +1,13 @@
+
+
+/*Learning Javaascript Data Types with code examples and explanations*/
+
+/*Data Types in Javascript are divided into two categories:
+1. Primitive Data Types
+2. Non-Primitive Data Types
+*/
+
+//1. Primitive Data Types
+/*Primitive Data Types are immutable and stored directly in the location that the variable accesses.
+When you access a primitive data type, you work on the actual value.*/
+
