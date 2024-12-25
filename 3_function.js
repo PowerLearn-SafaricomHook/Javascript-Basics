d3fca61a842f4031a5c77941efcc6c012a05201d
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 11:22:00 +0300

    add: implement function types and examples in 3_function.js

---
+++ 3_function.js
@@ -0,0 +1,42 @@
+
+
+//Functions in Javascript
+//Functions in Javascript are blocks of code that perform a specific task.
+//Functions are declared using the function keyword.
+//Functions can take parameters and return values.
+//Functions can be declared and called in different ways.
+//Functions can be assigned to variables.
+//Functions can be passed as arguments to other functions.
+//Functions can be returned from other functions.
+//Functions can be declared inside other functions.
+//Functions can be declared as arrow functions.
+
+/*Function Types
+There  types of functions in Javascript:
+1. Named Functions
+2. Arrow Functions
+3. Anonymous Functions*/
+
+//1. Named Functions
+/*Named functions are declared using the function keyword followed by the function name.*/
+function greet() {
+    console.log("Hello World");
+}
+greet(); //Hello World
+
+//2. Arrow Functions
+
+/*Arrow functions are declared using the => operator.*/
+let greet = () => {
+    console.log("Hello World");
+}
+greet(); //Hello World
+
+//3. Anonymous Functions
+/*Anonymous functions are declared without a name.*/
+let greet = function() {
+    console.log("Hello World");
+}
+greet(); //Hello World
+
+
