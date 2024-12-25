59348ce0ba43cf6521ff2d12fbfc7630d856e0b1
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 10:46:08 +0300

    add examples and explanations for primitive data types in 2_DataTypes.js

--- 2_DataTypes.js
+++ 2_DataTypes.js
@@ -11,3 +11,16 @@
 /*Primitive Data Types are immutable and stored directly in the location that the variable accesses.
 When you access a primitive data type, you work on the actual value.*/
 
+/*Primitive Data Types include:
+1. Number
+2. String
+3. Boolean
+4. Undefined
+5. Null
+6. Symbol
+*/
+
+//1. Number
+/*Number represents both integer and floating point numbers.*/
+let num = 10;
+// console.log(typeof num); //number
